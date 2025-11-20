06cdb05b6d789b65854d0d97d632275f1eb47fb4
Matiaslagost7 <matiaslagost7@gmail.com>
2025-11-20 16:45:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Scroll suave a secciones (mantiene funcionalidad personalizada)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+        });
+    });
+});
